c49dfcdb1a1ed014d15ae79e358537e57c683cac
adderxll <aardhapurkar22@gmail.com>
2025-06-24 14:00:09 +0530

    added hosts to the config for ngrok

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: [
+      "ba1c-2402-e280-3e38-203-511a-ee6f-4181-8bb7.ngrok-free.app",
+    ],
+  },
+});
